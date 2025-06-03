43992875187ec640d7266f634b4878cae5facd06
Daniel <daniel.guo@vivalabs.com.au>
2025-06-03 12:24:55 +1000

    Go by example: range

--- go-by-example/main.go
+++ go-by-example/main.go
@@ -1,16 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 // No additional imports needed since Hello and Values
 // are in the same package (main)
 
 func main() {
-	Slice()
-}
-
-func printLine(name string) {
-	fmt.Printf("------------%s------------------\n\n", name)
+	Range()
 }
--- go-by-example/range.go
+++ go-by-example/range.go
@@ -8,30 +8,29 @@ func Range() {
 	for _, num := range nums {
 		sum += num
 	}
-	fmt.Println("Sum of nums:", sum)
+	fmt.Println("Sum of nums:", sum) // 31
 
 	// range on arrays and slices provides both the index and value for each entry
 	for i, num := range nums {
 		if num == 8 {
-			fmt.Println("index of 8:", i)
+			fmt.Println("index of 8:", i) // 3
 		}
 	}
 
 	// range on map iterates over key/value pairs
 	kvs := map[string]string{"a": "apple", "b": "banana"}
 	for k, v := range kvs {
-		fmt.Printf("%s -> %s\n", k, v)
+		fmt.Printf("%s -> %s\n", k, v) // a -> apple, b -> banana
 	}
 
 	// range can also iterate over just the keys of a map
 	for k := range kvs {
-		fmt.Println("key:", k)
+		fmt.Println("key:", k) // a, b
 	}
 
 	// range on strings iterates over Unicode code points
 	// the first value is the starting byte index of the rune and the second is the rune itself
 	for i, c := range "hello" {
-		fmt.Println("index:", i, "char:", c)
+		fmt.Println("index:", i, "char:", c) // 0, 104, 1, 101, 2, 108, 3, 108, 4, 111
 	}
-
 }
